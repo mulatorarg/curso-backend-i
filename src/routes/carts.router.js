626d1ecc92f7b7a2805c1476de7b5eaab113ce48
mulator <campogabriel@gmail.com>
2024-10-11 09:59:58 -0300

    mas

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -4,7 +4,7 @@ import ProductModel from '../models/product.model.js';
 
 const router = Router();
 
-// Listar productos de un carrito por ID
+// Listar carritos
 router.get('/', async (req, res) => {
   try {
     const cart = await CartModel.find().lean();
@@ -73,8 +73,113 @@ router.post('/:cid/product/:pid', async (req, res) => {
   }
 });
 
+// TODO: Actualizar lista de productos del carrito
+router.put('/:cid', async (req, res) => {
+
+  try {
+
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    const qty = parseInt(req.body.qty ?? 0); //el usuario puede enviar valores negativos, se controla despues
+
+    const cart = await CartModel.findById(cid);
+    if (!cart) return res.status(404).json({ status: 'error', message: 'Carrito no encontrado.' });
+
+    const product = await ProductModel.findById(pid).lean();
+    if (!product) return res.status(404).json({ status: 'error', message: 'Producto no encontrado.' });
+
+    if (qty <=0) return res.status(404).json({ status: 'error', message: 'La cantidad del producto debe ser mayor a 0 para modificar el carrito.' });
+
+    const productIndex = cart.products.findIndex(item => item.product._id.toString() == pid);
+
+    if (productIndex !== -1) {
+      cart.products[productIndex].quantity = qty;
+      //cart.products[productIndex].price = product.price;
+    } else {
+      cart.products.push({ product: product, quantity: 1, price: product.price });
+    }
+
+    await cart.save();
+
+    return res.status(200).json({ status: 'success', payload: cart, message: 'Producto agregado al Carrito correctamente.' });
+  } catch (error) {
+    console.log("Error al agregar PRoducto al Carrito: ", error.message);
+    return res.status(500).json({ status: 'error', message: 'Error al agregar PRoducto al Carrito: ' + error.message });
+  }
+});
+
 // Agregar un producto al carrito
 router.put('/:cid/product/:pid', async (req, res) => {
+
+  try {
+
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    const qty = req.body.qty ?? 0; //el usuario puede enviar valores negativos, se controla despues
+
+    const cart = await CartModel.findById(cid);
+    if (!cart) return res.status(404).json({ status: 'error', message: 'Carrito no encontrado.' });
+
+    const product = await ProductModel.findById(pid).lean();
+    if (!product) return res.status(404).json({ status: 'error', message: 'Producto no encontrado.' });
+
+    if (qty <=0) return res.status(404).json({ status: 'error', message: 'La cantidad del producto debe ser mayor a 0 para modificar el carrito.' });
+
+    const productIndex = cart.products.findIndex(item => item.product._id.toString() == pid);
+
+    if (productIndex !== -1) {
+      cart.products[productIndex].quantity = qty;
+      //cart.products[productIndex].price = product.price;
+    } else {
+      cart.products.push({ product: product, quantity: 1, price: product.price });
+    }
+
+    await cart.save();
+
+    return res.status(200).json({ status: 'success', payload: cart, message: 'Producto agregado al Carrito correctamente.' });
+  } catch (error) {
+    console.log("Error al agregar PRoducto al Carrito: ", error.message);
+    return res.status(500).json({ status: 'error', message: 'Error al agregar PRoducto al Carrito: ' + error.message });
+  }
+});
+
+// TODO: eliminar todos los productos del carrito
+router.delete('/:cid', async (req, res) => {
+
+  try {
+
+    const cid = req.params.cid;
+    const pid = req.params.pid;
+    const qty = req.body.qty ?? 0; //el usuario puede enviar valores negativos, se controla despues
+
+    const cart = await CartModel.findById(cid);
+    if (!cart) return res.status(404).json({ status: 'error', message: 'Carrito no encontrado.' });
+
+    const product = await ProductModel.findById(pid).lean();
+    if (!product) return res.status(404).json({ status: 'error', message: 'Producto no encontrado.' });
+
+    if (qty <=0) return res.status(404).json({ status: 'error', message: 'La cantidad del producto debe ser mayor a 0 para modificar el carrito.' });
+
+    const productIndex = cart.products.findIndex(item => item.product._id.toString() == pid);
+
+    if (productIndex !== -1) {
+      cart.products[productIndex].quantity = qty;
+      //cart.products[productIndex].price = product.price;
+    } else {
+      cart.products.push({ product: product, quantity: 1, price: product.price });
+    }
+
+    await cart.save();
+
+    return res.status(200).json({ status: 'success', payload: cart, message: 'Producto agregado al Carrito correctamente.' });
+  } catch (error) {
+    console.log("Error al agregar PRoducto al Carrito: ", error.message);
+    return res.status(500).json({ status: 'error', message: 'Error al agregar PRoducto al Carrito: ' + error.message });
+  }
+});
+
+// TODO: eliminar un producto del carrito
+router.delete('/:cid/product/:pid', async (req, res) => {
 
   try {
 
